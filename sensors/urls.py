1cbe70f96a35057634cc430c9c14596a743194d8
Your Name <you@example.com>
2020-02-26 23:30:17 +0300

    Transfer to RPi

--- sensors/urls.py
+++ sensors/urls.py
@@ -4,4 +4,4 @@ from . import views
 urlpatterns = [
     path('', views.home, name="sensors-home"),
     path('about/', views.about, name="sensors-about"),
-]
+    ]
--- sensors/views.py
+++ sensors/views.py
@@ -1,8 +1,41 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+sensors_data = [
+    {
+        'sensor_id': 'GPIO 16',
+        'date_time': '26.02.2020 18:08:01',
+        'humidity': '46',
+        'temperature': '23'
+    },
+    {
+        'sensor_id': 'GPIO 16',
+        'date_time': '26.02.2020 18:08:02',
+        'humidity': '67',
+        'temperature': '21'
+    },
+    {
+        'sensor_id': 'GPIO 12',
+        'date_time': '26.02.2020 18:08:03',
+        '': '57',
+        'temperature': '22'
+    },
+    {
+        'sensor_id': 'GPIO 25',
+        'date_time': '26.02.2020 18:08:05',
+        'humidity': '69',
+        'temperature': '23'
+    },
+
+]
+
+
 def home(request):
-    return HttpResponse("<h1>Sensors home</h1>")
+    context = {
+        'sensors': sensors_data
+    }
+    return render(request, 'sensors/home.html', context)
 
 def about(request):
-    return HttpResponse("<h1>About</h1>")
+    return render(request, 'sensors/about.html', {'title': 'About'})
+
